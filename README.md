928984052ccc4aab497e97ce88c58be060d9a129
Varshil-Shah <varshilashah09@gmail.com>
2021-03-30 18:36:11 +0530

    Initial Project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Distance Converter in JavaScript
+
+## Image:
+![Converter](Image/page.png)
+
+## Objective:
+To convert distance from one unit to another.
+
+## Use case:
+1. Useful for engineering measurements.
+
+## About the creator:
+### Varshil Shah
+Feel free to mail me at my email address [Varshil Shah](mailto:varshilshah1004+github@gmail.com "Varshil Shah") for any queries.
+
+>Work hard + Smart
+
---
+++ script.js
@@ -0,0 +1,58 @@
+var convertButton = document.getElementById("convert");
+var answer = document.getElementById("answer");
+const error = document.getElementById("error");
+
+const distanceValue = {
+  "mile-km": 1.60934,
+  "mile-m": 1609.34,
+  "km-mile": 0.621371,
+  "km-m": 1000,
+  "m-mile": 0.000621371,
+  "m-km": 0.001,
+};
+
+convertButton.addEventListener("click", () => {
+  calculate();
+});
+
+getConvertFrom = () => {
+  let convertFrom = document.getElementById("convertFrom").value;
+  console.log(convertFrom);
+  return convertFrom;
+};
+
+getConvertTo = () => {
+  let convertTo = document.getElementById("convertTo").value;
+  console.log(convertTo);
+  return convertTo;
+};
+
+getInputValue = () => {
+  let inputValue = document.getElementById("value").value;
+  console.log(inputValue);
+  return inputValue;
+};
+
+getResult = (concatString, value) => {
+  for (let val in distanceValue) {
+    if (concatString == val) {
+      let number = distanceValue[val];
+      return value * number;
+    }
+  }
+};
+
+calculate = () => {
+  let concatString = getConvertFrom() + "-" + getConvertTo();
+  let value = getInputValue();
+  if (getConvertFrom() == getConvertTo()) {
+    answer.innerHTML = 0;
+    error.innerHTML = "Please select different conversion";
+  } else if (value <= 0) {
+    error.innerHTML = "Value must be greater than zero";
+  } else {
+    error.innerHTML = "";
+    let ans = getResult(concatString, value);
+    answer.innerHTML = ans;
+  }
+};
